39808dea012125400b209c87a51f19277cd818ed
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-01 22:41:30 -0400

    add associations between posts and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  validates :name, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
