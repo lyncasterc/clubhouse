0a9631027388f9eb9feaaab90df6e71a9d85b77b
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-03 16:42:01 -0400

    add comment associations with post and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :name, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
