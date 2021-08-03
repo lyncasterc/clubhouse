a2d6c5e3fb9909aa7b311ca215555a8f56b59415
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-03 16:01:44 -0400

    add all crud actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,12 +14,15 @@ class PostsController < ApplicationController
     @post = current_user.posts.build
   end
 
+  def edit
+  end
+
   def create
     @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to root_path, notice: "Post created!" }
+        format.html { redirect_to @post, notice: "Post created!" }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -27,6 +30,27 @@ class PostsController < ApplicationController
       end
     end
   end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "Post updated!" }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Post deleted!" }
+      format.json { head :no_content }
+    end
+  end
+
 
   private
 
