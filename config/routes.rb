88e9f0ade053917e4947faae7d87b11fc97d56e1
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-03 19:16:50 -0400

    add comments to post show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  
+  resources :posts do
+    resources :comments
+  end
 
   root 'posts#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
