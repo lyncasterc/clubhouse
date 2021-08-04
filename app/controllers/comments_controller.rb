b039bf95f362c2c67d2b0093f78d6748a4ea59f5
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-03 22:16:10 -0400

    write destroy action for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -36,6 +36,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
+
   private
 
   def comment_params
