d5a3776896214fa611daff3e7145d80d47fd03ce
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-04 17:12:25 -0400

    create callbacks for getting tags in posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,29 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_and_belongs_to_many :tags
   validates :title, presence: true
   validates :body, presence: true
+
+  after_create do
+    post = Post.find_by(id: self.id)
+    hashtags = self.body.scan(/#\w+/)
+    hashtags.uniq.each do |hashtag|
+      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+      post.tags << tag
+    end
+  end
+
+
+  before_update do
+    post = Post.find_by(id: self.id)
+    post.tags.clear # remove all tags from post when updating it, add it again
+    hashtags = self.body.scan(/#\w+/)
+    hashtags.uniq.each do |hashtag|
+      tag = Tag.find_or_create_by(name: hashtag.downcase.delete('#'))
+      post.tags << tag
+    end
+  end
 end
+
+
