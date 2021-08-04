823d36a64e0b3c0fa5ff18d40813cdba30b30230
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-04 17:12:59 -0400

    write helpers for hashtags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def render_with_hashtags(body)
+    body.gsub(/#\w+/) { |word| link_to word, "/posts/hashtag/#{word.delete('#')}" }.html_safe
+  end
+
+  def trending_hashtags
+    tags = Post.all.sample(200).collect { |post| post.tags }.flatten
+    trending_tags = Hash.new(0)
+    tags.each {|tag| trending_tags[tag] += 1 }
+
+    trending_tags.sort_by { |tag, count| -count }.take(6).collect {|arr| arr.first.name}
+  end
 end
